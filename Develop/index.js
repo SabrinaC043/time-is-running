2195724212b3ac0bb3b5f16e0cff9fdeb8fc86ca
Sabrina Carrion <sabrinacarrion@Sabrinas-MBP.lan>
2022-08-25 10:37:17 -0500

    added baseline code and files for quiz

---
+++ Develop/index.js
@@ -0,0 +1,110 @@
+// console.log ("Hello Goggins!");
+
+// variable declarations
+// array with    0                                               1                                    2                index and 3 for length 
+var startQuiz= document.getElementsByClassName("btnstart"); 
+var questions= ["What is the name of David's autobiography?","Where did David Goggins grow up?","What was David's previous job before he joined the Navy SEALs?",];
+let button= document.createElement("goback");
+const submit = "";
+var highScore="";
+// var timer= number.input.value;
+// number= input.value;
+
+// what are we going to do with this?//
+
+
+document.getElementsByClassName("btnstart").startQuiz
+console.log("btnstart");
+document.getElementById("question1")
+document.getElementById("question2")
+document.getElementById("question3")
+
+
+if (startQuiz){
+questions
+.forEach((questions) => {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// function showHighScore() {}
+
+
+
+// // What does the user need to see when we append it?
+
+
+// document.body.appendChild(startQuiz);
+// document.body.appendChild(questions);
+// document.body.appendChild(button);
+// document.write("highScore");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add listener to Start Quiz
+startQuiz.addEventListener("click", startQuiz);
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# time-is-running
+# Time Is Running:
+
+
+<h1> User Story </h1>
+As an amazing coding boot camp student
+I want to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers and do better, as well as work on my progress. 
+
+<h1> Motivation </h1>
+<p> Competitiveness is a great motivator and will aid in deterring procrastination as a bootcamp student. </p>
+
+<h4> Issues </h4>
