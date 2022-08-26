b157083e649c7c3652ee76f9779993f5c70b05f8
Sabrina Carrion <sabrinacarrion@Sabrinas-MBP.lan>
2022-08-25 22:43:57 -0500

    working through timer construction countdown and identified what was breaking the event listener

--- Develop/index.js
+++ Develop/index.js
@@ -2,41 +2,66 @@
 
 // variable declarations
 // array with    0                                               1                                    2                index and 3 for length 
-var startQuiz= document.getElementsByClassName("btnstart"); 
-var questions= ["What is the name of David's autobiography?","Where did David Goggins grow up?","What was David's previous job before he joined the Navy SEALs?",];
-let button= document.createElement("goback");
+var startQuiz= document.getElementsByClassName(".btnstart"); 
+// let button= document.createElement("goback");
 const submit = "";
 var highScore="";
-// var timer= number.input.value;
+let buttonBack= document.getElementsByClassName("goback");
+var bodyEl= document.getElementsByTagName("body");
+var outOfTime= "You are out of time, the quiz has ended";
+var timer= 15;
+
 // number= input.value;
 
 // what are we going to do with this?//
+//a variable that is an array of object, each with a key value pair of: questions, answers, and the correct answer
+var questions= [ {
+    question: "What is the name of David's autobiography?",
+    answers: ["The Road to Somewhere", "Green Lights", "Finding Me", "Can't Hurt Me"],
+   onlyRight: "Can't Hurt Me",
+}, 
+{question: "What was David's previous job before he joined the Navy SEALs?",
+answers: ["Firefighter", "Security Officer", "Exterminator", "Computer Administrator" ],
+onlyRight:"Exterminator",
+},
+{
+question: "What SEAL team was David a part of?",
+answers: ["SEAL Team 5", "SEAL Team 3", "SEAL Team 2", "SEAL Team 7", ],
+onlyRight:"SEAL Team 5",
+},]
 
+function timerTick(){
+    var timer= setInterval(function () {
+    
+    timer--; 
+    timeEl.textContent="You have", +  timer + "seconds left";
+    if (timer===0) {
+        clearInterval(timer);
+        // stops execution of action at setInterval
+        outOfTime();
+  }
+ }, 15000);
+}
 
-document.getElementsByClassName("btnstart").startQuiz
-console.log("btnstart");
-document.getElementById("question1")
-document.getElementById("question2")
-document.getElementById("question3")
+timerTick();
 
+function startQuiz () {
 
-if (startQuiz){
-questions
-.forEach((questions) => {
-
-
+    
 }
 
 
 
 
 
+// document.getElementsByClassName("btnstart").startQuiz;
+// console.log("btnstart");
+// document.getElementById("question1");
+// document.getElementById("question2");
+// document.getElementById("question3");
+// bodyEl.appendChild("btnstart");
 
-
-
-
-
-// function showHighScore() {}
+// // function showHighScore() {}
 
 
 
@@ -107,4 +132,4 @@ questions
 
 
 // add listener to Start Quiz
-startQuiz.addEventListener("click", startQuiz);
+// window.startQuiz.addEventListener("click", "startQuiz")
